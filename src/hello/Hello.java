69e7599121858cea6448f7fe3df976d088590e76
JONESJULIANPROF <56853901+JONESJULIANPROF@users.noreply.github.com>
2020-02-04 12:15:13 -0500

    this is the first commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author canno
+ */
+import javax.swing.JOptionPane;
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+        System.out.println("This is line two??");
+        
+        //JOptionPane.showMessageDialog(null,"This is a pop up!");
+       }
+               
+        
+    }
+    
+
